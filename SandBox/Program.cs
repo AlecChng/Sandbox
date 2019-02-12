bce1472f99008c0d5c699a7c58ce3be47a194c22
Alec Cheung <alecchng@gmail.com>
2019-02-11 19:39:31 -0800

    First commit

---
+++ SandBox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SandBox
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
