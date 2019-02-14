dc45c89c4e3fde5680258a70da8b72be3b12bd0c
Alec Cheung <alecchng@gmail.com>
2019-02-13 23:16:48 -0800

    Question 8.1 Triple Step

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
-
-namespace SandBox
+﻿namespace SandBox
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tripleStep = new TripleStep();
+            var recursionOnly = tripleStep.TripleStepRecursionOnly(6);
+            var dynamicProgramming = tripleStep.TripleStepDynamicProgramming(6);
+            var memoization = tripleStep.TripleStepMemoization(6);
         }
     }
 }
---
+++ SandBox/TripleStep.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace SandBox
+{
+    /// <summary>
+    /// Cracking the Coding Interview 8.1, Triple Step
+    /// </summary>
+    public class TripleStep
+    {
+        public int TripleStepRecursionOnly(int numSteps)
+        {
+            if (numSteps < 1)
+            {
+                return 0;
+            }
+            else if (numSteps == 1)
+            {
+                return 1;
+            }
+            else if (numSteps == 2)
+            {
+                return 2;
+            }
+            else if (numSteps == 3)
+            {
+                return 4;
+            }
+            else
+            {
+                return TripleStepRecursionOnly(numSteps - 1) + TripleStepRecursionOnly(numSteps - 2) + TripleStepRecursionOnly(numSteps - 3);
+            }
+        }
+
+        public int TripleStepDynamicProgramming(int numSteps)
+        {
+            // Key = total steps, Value = number of permutations for reaching numSteps
+            var memo = new Dictionary<int, int>
+            {
+                { 1, 1 },
+                { 2, 2 },
+                { 3, 4 }
+            };
+
+            return TripleStepDynamicProgrammingHelper(numSteps, memo);
+        }
+        private int TripleStepDynamicProgrammingHelper(int numSteps, Dictionary<int, int> memo)
+        {
+            if (numSteps < 1)
+            {
+                return 0;
+            }
+            else if (memo.ContainsKey(numSteps))
+            {
+                return memo[numSteps];
+            }
+            else
+            {
+                return TripleStepDynamicProgrammingHelper(numSteps - 1, memo) + TripleStepDynamicProgrammingHelper(numSteps - 2, memo) + TripleStepDynamicProgrammingHelper(numSteps - 3, memo);
+            }
+        }
+
+        public int TripleStepMemoization(int numSteps)
+        {
+            var memo = new Dictionary<int, int>
+            {
+                { 1, 1 },
+                { 2, 2 },
+                { 3, 4 }
+            };
+
+            if (numSteps <= 0)
+            {
+                return 0;
+            }
+            else if (numSteps < 4)
+            {
+                return memo[numSteps];
+            }
+
+            for (int i = 4; i <= numSteps; i++)
+            {
+                memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+            }
+
+            return memo[numSteps];
+        }
+    }
+}
