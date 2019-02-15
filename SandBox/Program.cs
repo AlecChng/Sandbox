5d2d0dc41577bafdf0f62b8ba57bdd532e8e67c4
Alec Cheung <alecchng@gmail.com>
2019-02-14 23:12:48 -0800

    Test

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -7,6 +7,52 @@ namespace SandBox
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //var states = new int[] { 1, 0, 0, 0, 0, 1, 0, 0 };
+            var states = new int[] { 1, 1, 1, 0, 1, 1, 1, 1 };
+            
+            var result = CellCompete(states, 2);
+        }
+
+        public static int[] CellCompete(int[] states, int days)
+        {
+            var leftCell = -1;
+            var rightCell = -1;
+
+            for (int currDay = 0; currDay < days; currDay++)
+            {
+                var nextDayStates = new int[states.Length];
+                for (int cellIndex = 0; cellIndex < states.Length; cellIndex++)
+                {
+                    if (cellIndex == 0)
+                    {
+                        leftCell = 0;
+                        rightCell = states[cellIndex + 1];
+                    }
+                    else if (cellIndex == states.Length - 1)
+                    {
+                        leftCell = states[cellIndex - 1];
+                        rightCell = 0;
+                    }
+                    else
+                    {
+                        leftCell = states[cellIndex - 1];
+                        rightCell = states[cellIndex + 1];
+                    }
+
+                    if (leftCell == rightCell)
+                    {
+                        nextDayStates[cellIndex] = 0;
+                    }
+                    else
+                    {
+                        nextDayStates[cellIndex] = 1;
+                    }
+                }
+                states = nextDayStates;
+            }
+
+            return states;
         }
     }
 }
