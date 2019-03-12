9f3921bd3936246ad1401d18346132cb8252095b
Alec Cheung <alecchng@gmail.com>
2019-03-11 20:24:58 -0700

    Scratch for Amazon_OA

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SandBox
 {
@@ -6,7 +7,19 @@ namespace SandBox
     {
         public static void Main(string[] args)
         {
+            int[,] test = new int[,] { { 1, 2 }, { 3, 4 }, { 1, -1 } };
+
+            var result = test[0,0];
+            var listTest = new List<int> { 1, 3, 2 };
+            listTest.Add
+            var dict = new Dictionary<int, List<List<int>>>();
+            dict.Add()
             Console.WriteLine("Hello World!");
         }
+
+        //public static void Test()
+        //{
+        //    Math.Sqr
+        //}
     }
 }
