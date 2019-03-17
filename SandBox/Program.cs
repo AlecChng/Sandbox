bbd6e33708833c6c84020132a1a263c34d7f4fc4
Alec Cheung <alecchng@gmail.com>
2019-03-17 16:55:37 -0700

    Implemented RobinInAGrid question 8.2

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -4,10 +4,54 @@
     {
         public static void Main(string[] args)
         {
-            var tripleStep = new TripleStep();
-            var recursionOnly = tripleStep.TripleStepRecursionOnly(6);
-            var dynamicProgramming = tripleStep.TripleStepDynamicProgramming(6);
-            var memoization = tripleStep.TripleStepMemoization(6);
+            //var tripleStep = new TripleStep();
+            //var recursionOnly = tripleStep.TripleStepRecursionOnly(6);
+            //var dynamicProgramming = tripleStep.TripleStepDynamicProgramming(6);
+            //var memoization = tripleStep.TripleStepMemoization(6);
+
+
+            var robotInAGrid = new RobotInAGrid();
+
+
+            // All off limts
+            //var grid = new bool[2, 2]; 
+
+            // 0,0 -> 1,0 -> 1,1 
+            //var grid = new bool[,]
+            //{
+            //    { true, true },
+            //    { true, true }
+            //};
+
+            // 0,0 -> 0,1 -> 1,1
+            //var grid = new bool[,]
+            //{
+            //    { true, true },
+            //    { true, true }
+            //};
+            //grid[1, 0] = false;
+
+
+            var grid = new bool[4, 4];
+            SetAllTrue(grid);
+            grid[1, 1] = false;
+            grid[2, 1] = false;
+            grid[3, 1] = false;
+            grid[2, 3] = false;
+
+            var path = robotInAGrid.FindRobotPath(grid);
+
+        }
+
+        private static void SetAllTrue(bool[,] grid)
+        {
+            for (var row = 0; row < grid.GetLength(0); row++)
+            {
+                for (var col = 0; col < grid.GetLength(1); col++)
+                {
+                    grid[row, col] = true;
+                }
+            }
         }
     }
 }
---
+++ SandBox/RobotInAGrid.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace SandBox
+{
+    public class RobotInAGrid
+    {
+        public LinkedList<(int XCoord, int YCoord)> FindRobotPath(bool[,] grid)
+        {
+            var path = new LinkedList<(int XCoord, int YCoord)>();
+            FindRobotPathHelper(grid, 0, 0, path);
+            return path;
+        }
+
+        private bool FindRobotPathHelper(bool[,] grid, int xCoord, int yCoord, LinkedList<(int XCoord, int YCoord)> path)
+        {
+            var numRows = grid.GetLength(0);
+            var numCols = grid.GetLength(1);
+            // Index Out of Bound
+            if (xCoord >= numCols || yCoord >= numRows)
+            {
+                return false;
+            }
+
+            var validCell = grid[xCoord, yCoord];
+            if (!validCell)
+            {
+                return false;
+            }
+            else if (xCoord == numCols - 1 && yCoord == numRows - 1)
+            {
+                path.AddLast((xCoord, yCoord));
+                return true;
+            }
+            else
+            {
+                var validPath = FindRobotPathHelper(grid, xCoord + 1, yCoord, path);
+                if (validPath)
+                {
+                    path.AddFirst((xCoord, yCoord));
+                }
+                else
+                {
+                    validPath = FindRobotPathHelper(grid, xCoord, yCoord + 1, path);
+                    if (validPath)
+                    {
+                        path.AddFirst((xCoord, yCoord));
+                    }
+                }
+
+                return validPath;
+            }
+        }
+    }
+}
